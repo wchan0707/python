a007d590e8987dbce287be2c470a452de63c6cfb
wchan0707 <wchan0707@gmail.com>
2020-04-26 14:07:57 +0900

    2020-04-26

--- getApple.py
+++ getApple.py
@@ -19,69 +19,56 @@ if __name__ == '__main__':
 	dbgfile = "Apple" + str(today.year) + ".debug.csv"
 	csvfile = "Apple" + str(today.year) + ".csv"
 
+	#オプションを設定する
 	options = Options()
-
 	options.set_headless(False)		#True: 画面表示なし
 
 	#Chromeを起動
 	driver = webdriver.Chrome(chrome_options=options)
 
 	#URLを開く
+	wait= 3
 	driver.get(url)
-	time.sleep(3)
-	print('Please wait 3sec...')
-
+	time.sleep(wait)
+	print('Please wait', wait, 'sec...')
 	data = driver.page_source.encode('utf-8')
-
 	doc = soup(data, 'html.parser')
 
-	params = [i for a in doc.find_all('dl') for i in a.find_all('dt') if i]
-	string2=list()
+	#パラメータを収集する
+	prmtnl= [
+		 ['市場', 			'#main > div.stockMainTab.clearFix > div.stockMainTabParts.stockMainTabPartsCurrent > span']
+		,['業種',			'#main > div.stocksDtlWp > div > div.forAddPortfolio > dl > dd.category.yjSb']
+		,['銘柄コード',		'#main > div.stocksDtlWp > div > div.forAddPortfolio > dl > dt']
+		,['銘柄名',			'#main > div.stocksDtlWp > div > div.forAddPortfolio > table > tbody > tr > th > h1']
+		,['日時',			'#main > div.stocksDtlWp > div > div.forAddPortfolio > dl > dd.yjSb.real']
+		,['株価',			'#main > div.stocksDtlWp > div > div.forAddPortfolio > table > tbody > tr > td:nth-child(3)']
+		,['前日比',			'#main > div.stocksDtlWp > div > div.forAddPortfolio > table > tbody > tr > td.change > span.icoUpGreen.yjMSt']
+		,['前日終値','#main > div.marB2.chartFinance.clearFix > div.innerDate > div:nth-child(1) > dl > dd > strong']
+		,['始値','']
+		,['高値','']
+		,['出来高','']
+		,['売買代金','']
+		,['時価総額','']
+		,['発行済株式数','']
+		,['','']
+		,['','']
+		,['','']
+		,['','']
+		,['','']
+	]
 
-	for num, link in enumerate(params, start=1):
-#		print(num, '[[[', link.text, ']]]')
-		print(num, '[[[', link.text.split('\n')[0], ']]]')
-#		print('[[[', link.string, ']]]')
-		string = str(link.string)
-		string = string.replace('\xa0', '')
-		string = string.replace('\xa9', '')
-		string2.append(string)
+	for num, link in enumerate(prmtnl, start=1):
+			params = doc.select(link[1])
+			print(link[0], '[[[', params[0].text, ']]]')
 
 	print()
 	print()
 	print()
 
-	params = [i for a in doc.find_all('dd') for i in a.find_all('strong') if i]
-
-	string2=list()
-		
-	for num, link in enumerate(params, start=1):
-#		print(num, '[[[', link.text, ']]]')
-		print(num, '[[[', link.string, ']]]')
-		string = str(link.string)
-		string = string.replace('\xa0', '')
-		string = string.replace('\xa9', '')
-		string2.append(string)
-
-	print()
-	print()
-	print()
-
-	params = [i for i in doc.find_all("dt",{"class":"title"}) if i]
-	string2=list()
-
-	for num, link in enumerate(params, start=1):
-#		print('[[[', link.text, ']]]')
-		print(num, '[[[', link.text.split('\n')[0], ']]]')
-#		print('[[[', string.split('>')[-1].split('<a')[0], ']]]')
-#		print('[[[', link.string, ']]]')
-		string = str(link.string)
-		string = string.replace('\xa0', '')
-		string = string.replace('\xa9', '')
-		string2.append(string)
-
+	# Chromeを閉じる
 	driver.close()
 
+	#Csvファイルに保存する
 #		if os.path.exists(csvfile):
 #			f= open(csvfile, 'a', encoding='Shift_jis')
 #			writer = csv.writer(f, lineterminator='\n')
